8f5a62a93c83ef63c92c892835aee22b5ee1044b
fynn-bckr <fbecker879@icloud.com>
2025-09-01 17:21:43 +0200

    chore: Rename files

---
+++ src/jaxatari/games/jax_tennis_separate_files.py
@@ -0,0 +1,67 @@
+from jaxatari.environment import JaxEnvironment, EnvState, EnvObs, EnvInfo
+from jaxatari.games.tennis_main import TennisState
+from typing import NamedTuple, Tuple
+import jax.numpy as jnp
+
+from jaxatari.renderers import JAXGameRenderer
+from tennis_main import tennis_step, tennis_reset
+from tennis_renderer import TennisRenderer as tr
+
+renderer = tr()
+
+
+class TennisObs(NamedTuple):
+    pass
+
+
+class TennisInfo(NamedTuple):
+    pass
+
+
+class AtraJaxisTennisRenderer(JAXGameRenderer):
+
+    def render(self, state: TennisState):
+        return renderer.render(state)
+
+
+class TennisConstants(NamedTuple):
+    pass
+
+
+class TennisJaxEnv(JaxEnvironment[TennisState, TennisObs, TennisInfo, TennisConstants]):
+
+    def reset(self, key) -> Tuple[TennisObs, TennisState]:
+        reset_state = tennis_reset()
+        reset_obs = self._get_observation(reset_state)
+
+        return reset_obs, reset_state
+
+    def step(self, state: TennisState, action) -> Tuple[TennisObs, TennisState, float, bool, TennisInfo]:
+        new_state = tennis_step(state, action)
+        new_obs = self._get_observation(new_state)
+        reward = self._get_reward(state, new_state)
+        done = self._get_done(new_state)
+        info = self._get_info(new_state)
+
+        return new_obs, new_state, reward, done, info
+
+    def render(self, state: TennisState) -> Tuple[jnp.ndarray]:
+        return renderer.render(state)
+
+    def action_space(self) -> jnp.ndarray:
+        return jnp.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+
+    def get_observation_space(self) -> Tuple:
+        pass
+
+    def _get_observation(self, state: TennisState) -> TennisObs:
+        return TennisObs()
+
+    def _get_info(self, state: TennisState) -> TennisInfo:
+        return TennisInfo()
+
+    def _get_reward(self, previous_state: TennisState, state: TennisState) -> float:
+        return 0.0
+
+    def _get_done(self, state: TennisState) -> bool:
+        return False
