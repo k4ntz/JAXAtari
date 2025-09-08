6af9e094703564f525d1a13b9ae550ea60018a40
Dominik Mandok <157524091+dominikmandok@users.noreply.github.com>
2025-09-09 01:00:31 +0200

    pls collapse the previous comment 👉👈

--- src/jaxatari/games/jax_pong.py
+++ src/jaxatari/games/jax_pong.py
@@ -11,7 +11,7 @@ from jaxatari.rendering import jax_rendering_utils as jr
 from jaxatari.environment import JaxEnvironment, JAXAtariAction as Action
 # NA HÖÖÖR MAAAAL
 # Kong # imaging whatever i cant believe it
-# choinges lellek
+# SOOOOOOO
 # jetztz könnte es oiserst wild werden :D
 class PongConstants(NamedTuple):
     MAX_SPEED: int = 12
