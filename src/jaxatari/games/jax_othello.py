f45ad7d0fd13b749b1032e4f75f8d4a6eb6f176f
Sven Bergrath <sbergrat@smail.uni-koeln.de>
2025-09-21 05:33:50 +0200

    one large fix

--- src/jaxatari/games/jax_othello.py
+++ src/jaxatari/games/jax_othello.py
@@ -2798,7 +2798,7 @@ class JaxOthello(JaxEnvironment[OthelloState, OthelloObservation, OthelloInfo, O
                 field_id = state.field.field_id, #richtig? da ja eigentich array und nicht konkreter wert
                 field_color = state.field.field_color,                
             ),
-            field_choice_player=state.field_choice_player[0]*8+state.field_choice_player, #nutze 1d representation der Feldkoordinate
+            field_choice_player=state.field_choice_player,
         )
     
     @partial(jax.jit, static_argnums=(0,))
@@ -2836,10 +2836,10 @@ class JaxOthello(JaxEnvironment[OthelloState, OthelloObservation, OthelloInfo, O
             "player_score": spaces.Box(low=0, high=64, shape=(), dtype=jnp.int32),
             "enemy_score": spaces.Box(low=0, high=64, shape=(), dtype=jnp.int32),
             "field": spaces.Dict({
-                "field_id": spaces.Box(low=0, high=63, shape=(), dtype=jnp.int32), #richtig?, da ja eigentlich array und kein konkreter wert?
-                "field_color": spaces.Box(low=0, high=2, shape=(), dtype=jnp.int32),
+                "field_id": spaces.Box(low=0, high=63, shape=(8,8), dtype=jnp.int32), #richtig?, da ja eigentlich array und kein konkreter wert?
+                "field_color": spaces.Box(low=0, high=2, shape=(8,8), dtype=jnp.int32),
             }),
-            "field_choice_player":  spaces.Box(low=0, high=63, shape=(), dtype=jnp.int32), #zu 0d array mit index 0-63 array konvertiert
+            "field_choice_player":  spaces.Box(low=0, high=63, shape=(2,), dtype=jnp.int32), 
         })
 
     def image_space(self) -> spaces.Box:
