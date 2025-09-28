4aa9dea2cc20da663c8098e08242eb262900ece5
jonasnmn <jonas13neumann@t-online.de>
2025-09-28 10:40:26 +0200

    removed videopinball from core

--- src/jaxatari/core.py
+++ src/jaxatari/core.py
@@ -11,7 +11,6 @@ GAME_MODULES = {
     "kangaroo": "jaxatari.games.jax_kangaroo",
     "freeway": "jaxatari.games.jax_freeway",
     "breakout": "jaxatari.games.jax_breakout",
-    "videopinball": "jaxatari.games.jax_videopinball",
     # Add new games here
 }
 
