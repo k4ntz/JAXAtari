e1409847a53d8805136b0032bf6a978bf94622df
Steven <mkh.steven@gmail.com>
2025-10-05 22:59:00 -0400

    remove incorrect changes

--- src/jaxatari/core.py
+++ src/jaxatari/core.py
@@ -11,7 +11,6 @@ GAME_MODULES = {
     "kangaroo": "jaxatari.games.jax_kangaroo",
     "freeway": "jaxatari.games.jax_freeway",
     "breakout": "jaxatari.games.jax_breakout",
-    "battlezone": "jaxatari.games.jax_battlezone",
     # Add new games here
 }
 
