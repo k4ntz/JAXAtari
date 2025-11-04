9511a0a16f25dbbba3d82278fea7f5a13f7bdfed
fsncmbe <fsncmbe@gmail.com>
2025-11-04 20:30:01 +0100

    Removed initial pong remains, started implementing player_step

--- src/jaxatari/games/jax_defender.py
+++ src/jaxatari/games/jax_defender.py
@@ -10,33 +10,30 @@ import chex
 import jaxatari.spaces as spaces
 from jaxatari.renderers import JAXGameRenderer
 from jaxatari.rendering import jax_rendering_utils as render_utils
-from jaxatari.environment import JaxEnvironment, JAXAtariAction as Action, EnvObs, EnvState, EnvInfo
+from jaxatari.environment import (
+    JaxEnvironment,
+    JAXAtariAction as Action,
+    EnvObs,
+    EnvState,
+    EnvInfo,
+)
 from jaxatari.spaces import Space
 
 
 class DefenderConstants(NamedTuple):
     MAX_SPEED: int = 12
-    BALL_SPEED: chex.Array = jnp.array([-1, 1])
     ENEMY_STEP_SIZE: int = 2
     WIDTH: int = 160
     HEIGHT: int = 210
-    BASE_BALL_SPEED: int = 1
-    BALL_MAX_SPEED: int = 4
-    MIN_BALL_SPEED: int = 1
-    PLAYER_ACCELERATION: chex.Array = jnp.array([6, 3, 1, -1, 1, -1, 0, 0, 1, 0, -1, 0, 1])
-    BALL_START_X: chex.Array = jnp.array(78)
-    BALL_START_Y: chex.Array = jnp.array(115)
+    PLAYER_ACCELERATION: chex.Array = jnp.array(
+        [6, 3, 1, -1, 1, -1, 0, 0, 1, 0, -1, 0, 1]
+    )
     BACKGROUND_COLOR: Tuple[int, int, int] = (144, 72, 17)
     PLAYER_COLOR: Tuple[int, int, int] = (92, 186, 92)
-    ENEMY_COLOR: Tuple[int, int, int] = (213, 130, 74)
-    BALL_COLOR: Tuple[int, int, int] = (236, 236, 236)
-    WALL_COLOR: Tuple[int, int, int] = (236, 236, 236)
     SCORE_COLOR: Tuple[int, int, int] = (236, 236, 236)
     PLAYER_X: int = 140
-    ENEMY_X: int = 16
-    PLAYER_SIZE: Tuple[int, int] = (4, 16)
-    BALL_SIZE: Tuple[int, int] = (2, 4)
-    ENEMY_SIZE: Tuple[int, int] = (4, 16)
+    PLAYER_Y: int = 80
+    PLAYER_SIZE: Tuple[int, int] = (16, 16)
     WALL_TOP_Y: int = 24
     WALL_TOP_HEIGHT: int = 10
     WALL_BOTTOM_Y: int = 194
@@ -45,16 +42,8 @@ class DefenderConstants(NamedTuple):
 
 # immutable state container
 class DefenderState(NamedTuple):
-    player_y: chex.Array
     player_speed: chex.Array
-    ball_x: chex.Array
-    ball_y: chex.Array
-    enemy_y: chex.Array
-    enemy_speed: chex.Array
-    ball_vel_x: chex.Array
-    ball_vel_y: chex.Array
-    player_score: chex.Array
-    enemy_score: chex.Array
+    score: chex.Array
     step_counter: chex.Array
     acceleration_counter: chex.Array
     buffer: chex.Array
@@ -69,25 +58,65 @@ class EntityPosition(NamedTuple):
 
 class DefenderObservation(NamedTuple):
     player: EntityPosition
-    enemy: EntityPosition
-    ball: EntityPosition
-    score_player: jnp.ndarray
-    score_enemy: jnp.ndarray
+    score: jnp.ndarray
 
 
 class DefenderInfo(NamedTuple):
     time: jnp.ndarray
 
 
-class JaxDefender(JaxEnvironment[DefenderState, DefenderInfo, DefenderInfo, DefenderConstants]):
+class JaxDefender(
+    JaxEnvironment[DefenderState, DefenderObservation, DefenderInfo, DefenderConstants]
+):
 
-    def reset(self, key: jrandom.PRNGKey=None) -> Tuple[EnvObs, EnvState]:
+    def __init__(self, consts: DefenderConstants = None):
+        consts = consts or DefenderConstants()
+        super().__init__(consts)
+        self.renderer = DefenderRenderer(self.consts)
+        self.action_set = [
+            Action.NOOP,
+            Action.FIRE,
+            Action.UP,
+            Action.RIGHT,
+            Action.LEFT,
+            Action.DOWN,
+            Action.UPRIGHT,
+            Action.UPLEFT,
+            Action.DOWNRIGHT,
+            Action.DOWNLEFT,
+            Action.UPFIRE,
+            Action.RIGHTFIRE,
+            Action.LEFTFIRE,
+            Action.DOWNFIRE,
+            Action.UPRIGHTFIRE,
+            Action.UPLEFTFIRE,
+            Action.DOWNRIGHTFIRE,
+            Action.DOWNLEFTFIRE,
+        ]
+
+    def _player_step(self, state: DefenderState, action: chex.Array) -> DefenderState:
+        up = action == Action.UP
+        down = action == Action.DOWN
+        left = action == Action.LEFT
+        right = action == Action.RIGHT
+
+        # touches wall here
+
+        player_speed = state.player_speed
+
+        # resume from pong here
+
         pass
 
-    def step(self, key: jrandom.PRNGKey=None) -> DefenderState:
+    def reset(self, key=None) -> Tuple[DefenderObservation, DefenderState]:
         pass
 
-    def render(self, key: jrandom.PRNGKey=None) -> Tuple[EnvObs, EnvState]:
+    @partial(jax.jit, static_argnums=(0,))
+    def step(self, key: jrandom.PRNGKey = None) -> DefenderState:
+
+        pass
+
+    def render(self, state: DefenderState) -> Tuple[DefenderObservation, DefenderState]:
         pass
 
     def action_space(self) -> spaces.Space:
@@ -105,11 +134,13 @@ class JaxDefender(JaxEnvironment[DefenderState, DefenderInfo, DefenderInfo, Defe
     def observation_spaces(self) -> spaces.Space:
         pass
 
-    def _get_info(self, state: EnvState, all_rewards: jnp.array = None) -> EnvInfo:
+    def _get_info(
+        self, state: DefenderState, all_rewards: jnp.array = None
+    ) -> DefenderInfo:
         pass
 
-    def _get_reward(self, previous_state: EnvState, state: EnvState) -> float:
+    def _get_reward(self, previous_state: DefenderState, state: DefenderState) -> float:
         pass
 
-    def _get_done(self, state: EnvState) -> bool:
+    def _get_done(self, state: DefenderState) -> bool:
         pass
