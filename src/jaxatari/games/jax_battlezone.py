c02583e04492d4009bb36d0311ec05045e060cb0
Tyfor <timo@his4sports.de>
2025-11-06 15:09:15 +0100

    check individual pixels by plotting observation

--- src/jaxatari/games/jax_battlezone.py
+++ src/jaxatari/games/jax_battlezone.py
@@ -1,15 +1,31 @@
 import gymnasium as gym
 import ale_py
 from gymnasium.utils import play
+import matplotlib.pyplot as plt
 
 gym.register_envs(ale_py)
 
-if __name__ == "__main__":
-    #print(sorted(gym.envs.registry.keys()))
-    env = gym.make("ALE/BattleZone-v5", render_mode="rgb_array", frameskip=1)
 
+def try_gym_battlezone():
+    env = gym.make("ALE/BattleZone-v5", render_mode="rgb_array", frameskip=1)
     # Reset the environment to generate the first observation
-    #trained_agent_test(env)
     observation, info = env.reset(seed=42)
-    play.play(env,zoom=3,fps=60)#, keys_to_action=keys_to_action)
+    play.play(env, zoom=3, fps=60)  # , keys_to_action=keys_to_action)
     env.close()
+
+def try_gym_battlezone_pixel():
+    env = gym.make("ALE/BattleZone-v5", render_mode="rgb_array", frameskip=1)
+    # Reset the environment to generate the first observation
+    observation, info = env.reset(seed=42)
+    stepsize = 100
+    for i in range(1000):
+        action = 3
+        obs, reward, terminated, truncated, info = env.step(action)
+        if i%stepsize==0:
+            im = plt.imshow(obs, interpolation='none', aspect='auto')
+            plt.show()
+    env.close()
+
+
+if __name__ == "__main__":
+    try_gym_battlezone()
