5e99c8dae16dbfbcaff65f2a8e0bc94d51ab5f91
FugeFF <yannick.fuge@hotmail.com>
2026-03-12 20:27:35 +0100

    Basic Mods implemented

---
+++ src/jaxatari/games/mods/basicmath_mods.py
@@ -0,0 +1,31 @@
+from jaxatari.modification import JaxAtariModController
+from jaxatari.games.mods.basicmath_mod_plugins import (
+    BackgroundBlackColorMod,
+    BackgroundRandomColorMod,
+    NumberRandomColorMod
+)
+
+class BasicMathEnvMod(JaxAtariModController):
+    """
+    Game-specific Mod Controller for Breakout.
+    It simply inherits all logic from JaxAtariModController and defines the BREAKOUT_MOD_REGISTRY.
+    """
+
+    REGISTRY = {
+        "background_black": BackgroundBlackColorMod,
+        "background_random": BackgroundRandomColorMod,
+        "number_random": NumberRandomColorMod
+    }
+
+    def __init__(self,
+                 env,
+                 mods_config: list = [],
+                 allow_conflicts: bool = False
+                 ):
+
+        super().__init__(
+            env=env,
+            mods_config=mods_config,
+            allow_conflicts=allow_conflicts,
+            registry=self.REGISTRY
+        )
