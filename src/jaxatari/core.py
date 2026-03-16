02c4d9cb4fda4e5c809d7023e928116a1a3b2a20
ahmetzahitak <126958359+ahmetzahitak@users.noreply.github.com>
2026-03-16 20:56:42 +0100

    Update core.py

--- src/jaxatari/core.py
+++ src/jaxatari/core.py
@@ -19,7 +19,6 @@ GAME_MODULES = {
     "breakout": "jaxatari.games.jax_breakout",
     "centipede": "jaxatari.games.jax_centipede",
     "choppercommand": "jaxatari.games.jax_choppercommand",
-    "crossbow": "jaxatari.games.jax_crossbow",
     "enduro": "jaxatari.games.jax_enduro",
     "fishingderby": "jaxatari.games.jax_fishingderby",
     "freeway": "jaxatari.games.jax_freeway",
@@ -61,7 +60,6 @@ MOD_MODULES = {
     "freeway": "jaxatari.games.mods.freeway_mods.FreewayEnvMod",
     "breakout": "jaxatari.games.mods.breakout_mods.BreakoutEnvMod",
     "seaquest": "jaxatari.games.mods.seaquest_mods.SeaquestEnvMod",
-    "crossbow": "jaxatari.games.mods.crossbow_mods.CrossbowEnvMod",
 }
 
 
