f4db1218ea7868b6bbd31208c99f3911922c7468
tiago-hfs <117849176+tiago-hfs@users.noreply.github.com>
2026-04-27 12:32:03 +0200

    Gravitar Rework: First Submission (#220)
    * Fixed loading of panet levels, fixed arena fight, fixed rammer behaviour, fixed gravity for each of the maps, added thrust blit, fixed fuel system
    
    * Green Bunkers now shoot green bullets
    
    * Added Tractor Beam Functionality and Fixed Shield
    
    * Manually leaving a level after hitting the win condition is now possible. Winning the solar system now adds the correct bonuses and the level will restart (future updates will add next levels)
    
    * Fix: Rammer Crash now kills the ship instead of the rammer
    
    * Fix: Fixed how planet 2 was displayed, fixed the sprite of the surface and coordinates of bunkers and tank; Additional fixes: no fuel reset after lost ship; removed -10 points penalty on kill
    
    * Rammer does not spawn on planet 2 anymore
    
    * Fix: UFOs can now respawn and don't have to be killed anymore to advance level
    
    * Prepare submission
    
    * Remove old experimental file for submission
    
    * Fix: reactor Destination now causes crash + Saucer behaviour in solar system modified (only x-axis movement + more bullets) + Crashing into the saucer in arena mode no longer kills the saucer
    
    * Fix: Overflow Problem is fixed + Reactor Level can be left early ; Preparation: Prepared spaceship sprites for different angles
    
    * Introducing new ship_rotate mechanism + first overflow fix attempt + refactoring of constants
    
    * Preserve ship position after arena battle
    
    * Solar system item location fixed
    
    * Fixed respawn position for all respective levels
    
    * Fix: ship now loses fuel in arena level + respawn at loss is now correct
    
    * Minor hitbox fix
    
    * Refactoring after repository update
    
    * some tweaks to parameters to increase similarity to ALE
    
    * Fixed downscaling error
    
    * Adjustment of physics details to increase similarity to ALE + Adjustment of offsets to rectify positioning of sprites
    
    * UFO Spawn logic adjusted + Further adjustments to thrust power and bouncing effect
    
    * Slight ship starting position adjustments
    
    * Adjusted Bullet Speeds and some positioning + Removed unnecessary pngs + prepared sprites of planets for level 2
    
    * Updated death logic for ship and ufo
    
    * Minor Rework of arena battle (spawn positions, movement trajectory when entering and leaving)
    
    * Fix: Modify Death Logic + change player bullet logic
    
    * Added Mod Support and RapidFire Mod + Adjusted jax_gravitar logic to allow simpler modding
    
    * Add ZeroGravity and HyperGravity Mods
    
    * Refactor to conform to Repo Updates "States, observations, and info must now be implemented as flax.struct.dataclasses."
    
    * Added Fuel Crisis Mod
    
    * Slight adjustment to pass tests
    
    * Another minor fix attempt to pass CI tests
    
    * Another attempt at modifying death/reset logic to pass CI tests
    
    * Adjusted death logic once again
    
    * Added HarmlessEnemiesMod and ValuableReactorMod + Green Enemies now have hidden Fuel Tanks + Renaming "terrant" to "terrain"
    
    * Next CI Test fix attempt
    
    * next attempt at fixing "done" logic
    
    * another death logix fix attempt
    
    * Another attempt to fix done flag
    
    * Another attempt to fix "done" logic
    
    * another attempt at fixing "done"
    
    * Adjusted jax data type usage
    
    * More jax datatype adjustments
    
    * Added AntiGravityMod and attempt to fix LogWrapper Error
    
    * Add HighSpeedMod and modify Observations to include more relevant parameters
    
    * Some minor adjustments to mods and reward system
    
    * Several fixes exposed by first training results: 1) Losing your last life in planet level mode now causes episode end and reset, 2) Having 0 fuel now causes Episode end and reset, 3) The shield/tractor action no longer influences ship movement
    
    * Some Observation Fixes
    
    * added sprites for future level 2
    
    * final observation state changes (simplification) that were made before training
    
    * FINAL UPDATE: Slight performance improvement

---
+++ src/jaxatari/games/mods/gravitar/gravitar_mod_plugins.py
@@ -0,0 +1,83 @@
+from jaxatari.modification import JaxAtariInternalModPlugin
+
+
+class RapidFireMod(JaxAtariInternalModPlugin):
+    """Increase active bullet caps for ship, saucer, and enemies to 4."""
+
+    constants_overrides = {
+        "MAX_ACTIVE_PLAYER_BULLETS_MAP": 4,
+        "MAX_ACTIVE_PLAYER_BULLETS_LEVEL": 4,
+        "MAX_ACTIVE_PLAYER_BULLETS_ARENA": 4,
+        "MAX_ACTIVE_SAUCER_BULLETS": 4,
+        "MAX_ACTIVE_ENEMY_BULLETS": 8,  # 2 bullets per enemy * 4 enemies
+    }
+
+
+class ZeroGravityMod(JaxAtariInternalModPlugin):
+    """Disable all gravity from sun, planets, and reactors."""
+
+    constants_overrides = {
+        "SOLAR_GRAVITY": 0.0,
+        "PLANETARY_GRAVITY": 0.0,
+        "REACTOR_GRAVITY": 0.0,
+    }
+
+
+class HyperGravityMod(JaxAtariInternalModPlugin):
+    """Increase all gravity from sun, planets, and reactors substantially."""
+
+    constants_overrides = {
+        "SOLAR_GRAVITY": 0.132,  # 0.044 * 3
+        "PLANETARY_GRAVITY": 0.0096,  # 0.0032 * 3
+        "REACTOR_GRAVITY": 0.001,  # 0.0001 * 10
+    }
+
+
+class FuelCrisisMod(JaxAtariInternalModPlugin):
+    """Increase fuel consumption rate by 5x."""
+
+    constants_overrides = {
+        "FUEL_CONSUME_THRUST": 20.0,
+        "FUEL_CONSUME_SHIELD_TRACTOR": 50.0,
+    }
+
+class HarmlessEnemiesMod(JaxAtariInternalModPlugin):
+    """Make all enemies harmless by disabling their bullets."""
+
+    constants_overrides = {
+        "MAX_ACTIVE_ENEMY_BULLETS": 0,
+        "MAX_ACTIVE_SAUCER_BULLETS": 0,
+    }
+
+
+class ValuableReactorMod(JaxAtariInternalModPlugin):
+    """Populate reactor level with 3 enemies and 2 fuel tanks."""
+
+    constants_overrides = {
+        "ALLOW_TRACTOR_IN_REACTOR": True,
+        "REACTOR_LEVEL_LAYOUT": (
+            {"type": 5, "coords": (104, 104)},   # ENEMY_ORANGE
+            {"type": 6, "coords": (56, 144)},   # ENEMY_GREEN
+            {"type": 39, "coords": (80, 18)},  # ENEMY_ORANGE_FLIPPED
+            {"type": 12, "coords": (66, 88)}, # FUEL_TANK
+        ),
+    }
+
+
+class AntiGravityMod(JaxAtariInternalModPlugin):
+    """Reverse gravity from sun, planets, and reactors."""
+
+    constants_overrides = {
+        "SOLAR_GRAVITY": -0.005,
+        "PLANETARY_GRAVITY": -0.0032,
+        "REACTOR_GRAVITY": -0.0001,
+    }
+
+
+class HighSpeedMod(JaxAtariInternalModPlugin):
+    """Make the ship faster by increasing thrust power and max speed."""
+
+    constants_overrides = {
+        "THRUST_POWER": 0.075,
+        "MAX_SPEED": 6.0,
+    }
