9ea9e1620711bf57a2112de8bd69b69959e7aacc
janli <janlin1805@gmail.com>
2026-05-26 23:05:10 +0200

    Skeleteon for Game class

--- src/jaxatari/games/jax_crazy_climber.py
+++ src/jaxatari/games/jax_crazy_climber.py
@@ -1,6 +1,10 @@
 from flax import struct
 
-from jaxatari.environments import JaxEnvironment
+import jax
+import jax.numpy as jnp
+
+from jaxatari.spaces import spaces
+from jaxatari.environment import JaxEnvironment
 
 class CrazyClimberState(struct.PyTreeNode):
     pass
@@ -15,4 +19,18 @@ class CrazyClimberConstants(struct.PyTreeNode):
     pass
 
 class JaxCrazyClimber(JaxEnvironment[CrazyClimberState, CrazyClimberObservation, CrazyClimberInfo, CrazyClimberConstants]):
-    pass
+    def __init__(self, consts: CrazyClimberConstants):
+        super().__init__(consts)
+
+    def reset(self, key: jax.random.PRNGKey) -> (CrazyClimberObservation, CrazyClimberState):
+        pass
+
+    def step(self, state: CrazyClimberState, action: int) -> (CrazyClimberObservation, CrazyClimberState, float, bool, CrazyClimberInfo):
+        pass
+
+    def render(self, state: CrazyClimberState) -> jnp.ndarray:
+        pass
+
+    # TODO: Returntype needs to be altered to match actual implementation
+    def action_space(self) -> spaces.Discrete:
+        pass
