33e0dad93a65a3ada7c023c06f47033ad1563f1c
crafter23456 <64259198+crafter23456@users.noreply.github.com>
2026-09-13 19:27:01 +0200

    remove weird comment

--- src/jaxatari/games/jax_demonattack.py
+++ src/jaxatari/games/jax_demonattack.py
@@ -2501,7 +2501,7 @@ class JaxDemonAttack(JaxEnvironment[DemonAttackState, DemonAttackObservation, De
             demon_split_primary_alive=jnp.where(killed, False, demon_split_primary_alive),
             demon_split_secondary_alive=jnp.where(killed, False, demon_split_secondary_alive),
             demon_status=demon_status,
-            demon_mode=jnp.where(killed, BEHAVIOR_NORMAL, state.demon_mode),  # <-- add this
+            demon_mode=jnp.where(killed, BEHAVIOR_NORMAL, state.demon_mode),
             demon_phase=jnp.where(killed, 0, state.demon_phase),
             demon_moving_right=jnp.where(killed, False, state.demon_moving_right),
             demon_moving_down=jnp.where(killed, True, state.demon_moving_down),
